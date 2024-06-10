bd3c04cc32ecaa2a59d5f149fa0fbc02f53cbc3d
spinelf@gmail.com <spinelf@gmail.com>
2024-06-10 18:17:36 +0200

    Subida mejor interfaz y apertura de informes

--- Fase05_Creación_de_una_ETL/ventana.py
+++ Fase05_Creación_de_una_ETL/ventana.py
@@ -18,9 +18,9 @@ class Ventana(Frame): #Clase ventana de tipo frame
     dao=DAO()
     
     def __init__(self, master=None):
-        super().__init__(master,width=1000,height=400)
+        super().__init__(master,width=1000,height=400,bg="#040452")
         self.master=master
-        self.pack()
+        self.pack(expand=True, fill="both")
         self.crear_widgets() #creamos todos los controles
    
        
@@ -82,8 +82,8 @@ class Ventana(Frame): #Clase ventana de tipo frame
       self.dao.cargar_datos_BBDD(qu.query_insertar_cv_details,'abc_corporation',datos_tabla_cv_details)
          
              
-    def fInformeResultados(self):
-        ruta_pdf = r"Fase06_Reporte_Resultados\InformeAnalisis.pdf"  # Cambia esto por la ruta de tu archivo PDF
+    def fInformeResultadospdf(self):
+        ruta_pdf = r"Fase06_Reporte_Resultados\InformeAnalisisAbcCorporation.pdf"  # Cambia esto por la ruta de tu archivo PDF
         try:
             if os.name == 'nt':  # Para sistemas Windows
                 os.startfile(ruta_pdf)
@@ -91,8 +91,19 @@ class Ventana(Frame): #Clase ventana de tipo frame
                 subprocess.call(('open', ruta_pdf) if sys.platform == 'darwin' else ('xdg-open', ruta_pdf))
         except Exception as e:
             print(f"Error al abrir el archivo PDF: {e}")
+            
+    def fInformeResultadosppt (self):
+        ruta_pptx = r"Fase06_Reporte_Resultados\ManualdeUsuario.pptx"  # Cambia esto por la ruta de tu archivo PDF
+        try:
+            if os.name == 'nt':  # Para sistemas Windows
+                os.startfile(ruta_pptx)
+            elif os.name == 'posix':  # Para sistemas Unix (Linux, macOS)
+                subprocess.call(('open', ruta_pptx) if sys.platform == 'darwin' else ('xdg-open', ruta_pptx))
+        except Exception as e:
+            print(f"Error al abrir el archivo PPT: {e}")
       
     # Falta implementación, se hará en versión 2.0      
+    
     '''def fmostrar_frame_mantenimiento(self):
         # Esconder otros frames si es necesario
         for widget in self.winfo_children():
@@ -123,29 +134,43 @@ class Ventana(Frame): #Clase ventana de tipo frame
     def crear_widgets(self):
        
         #primer cuadro de la izq
-        frame1=Frame(self, bg="#8A86B2")
-        frame1.place(x=0,y=0,width=200,height=399)
+        frame1=Frame(self, bg="#040452")
+        #frame1.place(x=0,y=0)
+        frame1.pack(side="top", fill="y")
         
-        #Boton Nuevo
+        # Frame para botones en horizontal
+        botones_frame = Frame(frame1, bg="#040452")
+        botones_frame.pack(side="top", padx=20)
+        
+        #Boton Nueva BBDD
         self.btnNuevaBBDD=Button(frame1,text="Crear BBDD y Tablas",command=self.fNuevaBBDDyTablas, bg="#3D3681", fg="white")
-        self.btnNuevaBBDD.place(x=15,y=125,width=150,height=40)
+        self.btnNuevaBBDD.pack(side="left", padx=10, pady=60)
         
-        #Boton Nuevo
-        self.btnNuevaBBDD=Button(frame1,text="Cargar Datos",command=self.fCargarDatos, bg="#3D3681", fg="white")
-        self.btnNuevaBBDD.place(x=15,y=175,width=150,height=40)    
+        
+        #Boton CargarDatos
+        self.btnCargarDatos=Button(frame1,text="Cargar Datos",command=self.fCargarDatos, bg="#3D3681", fg="white")
+        self.btnCargarDatos.pack(side="left", padx=10, pady=10)
+        
+        #Boton AnalisisDeDatos
+        self.btnAnalisisDatos=Button(frame1,text="Informe Resultados ",command=self.fInformeResultadospdf, bg="#3D3681", fg="white")
+        self.btnAnalisisDatos.pack(side="left", padx=10, pady=10)
+        
+        #Boton ManualUsuario
+        self.btnManual=Button(frame1,text="Manual de Usuario ",command=self.fInformeResultadosppt, bg="#3D3681", fg="white")
+        self.btnManual.pack(side="left", padx=10, pady=10)
               
-        #Boton Nuevo
+        #Boton Nuevo, falta implementar en la versión 2.0
+        
         #self.btnMantenimiento=Button(frame1,text="Mantenimiento empleados",command=self.fmostrar_frame_mantenimiento, bg="#3D3681", fg="white")
         #self.btnMantenimiento.place(x=15,y=200,width=150,height=40) 
         
-        #Boton Nuevo
-        self.btnAnalisisDatos=Button(frame1,text="Informe Resultados ",command=self.fInformeResultados, bg="#3D3681", fg="white")
-        self.btnAnalisisDatos.place(x=15,y=225,width=150,height=40) 
+        #FIN DE FALTA IMPLEMENTACION.
+              
+        #El frame 2 con la imagen de la empresa
         
-        #el frame 2 con la imagen de la empresa
-        
-        frame2=Frame(self, bg="#040452")
-        frame2.place(x=200,width=800,height=399)
+        #frame2=Frame(self, bg="#040452")
+        #frame2.pack(side="right", expand=True, fill="both")
+        #frame2.place(x=200,width=800,height=399)
          
         # Cargar la imagen
         self.image_path=r"images\logo_ABC.png"
@@ -153,8 +178,13 @@ class Ventana(Frame): #Clase ventana de tipo frame
         self.photo = ImageTk.PhotoImage(self.image)
         
         # Crear un label para la imagen y centrarlo
-        self.logo_label = Label(self, image=self.photo)
-        self.logo_label.place(relx=0.5, rely=0.5, anchor=CENTER)
+        self.logo_label = Label(self, image=self.photo,bg="#040452")
+        self.logo_label.place(relx=0.5, rely=0.5, anchor="center")
+        
+        '''self.logo_label.pack(expand=True)
+        self.logo_label.pack_propagate(False)
+        self.logo_label.update_idletasks()
+        self.logo_label.pack(anchor="center")'''
         
       # Frame para el mantenimiento de empleados, se implementará en la versión 2.0
         '''self.frame_mantenimiento = Frame(self, bg="#040452")
@@ -167,8 +197,5 @@ class Ventana(Frame): #Clase ventana de tipo frame
         
         self.boton_modificar = Button(self.frame_mantenimiento, text="Modificar", command=self.fmodificar_empleado, bg="#3D3681", fg="white")
         self.boton_modificar.place(x=15, y=150, width=150, height=40)'''
-        
-
-        
-  
+       
      
--- main.py
+++ main.py
@@ -7,6 +7,10 @@ from Fase05_Creación_de_una_ETL.ventana import *
 def main():
     root=Tk() #crear objeto del tipo Tk
     root.wm_title("ABC Corporation")
+    root.configure(bg="#040452")
+    # Ajustar la ventana principal para maximizarse
+    root.state('zoomed')
+   
     app=Ventana(root) #Creamos una clase de tipo ventana.
     app.mainloop()
    
